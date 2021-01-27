5c23a50d6c3bd196ec0db8d42df445366e3682a8
hsz75 <72518650+hsz75@users.noreply.github.com>
2021-01-27 10:39:12 +0800

    Update nowpath.js

--- f/j/nowpath.js
+++ f/j/nowpath.js
@@ -1,4 +1,3 @@
 var nowpath=location.pathname;
-function DisplayNowPath(){
-	document.getElementById("nowpath").innerHTML='当前访问'+nowpath;
-}
+document.getElementById("nowpath").innerHTML='当前访问'+nowpath;
+
