65d134e0eebeed5a235dd70212be07b9458da903
hsz75 <hsz75@qq.com>
2021-01-30 23:23:51 +0800

    Update log.js

--- f/j/log.js
+++ f/j/log.js
@@ -1,2 +1,4 @@
-console.log("Copyright 2020-2021 Dnyyfb All Right Reserved. ")
+console.log("Copyright 2020-2021 hsz75 All Right Reserved. ")
 console.warn("Without permission, it is forbidden to carry any code of this website!")
+console.warn("这是一个警告，也是一个彩蛋")
+console.warn("未经允许，禁止复制代码")
