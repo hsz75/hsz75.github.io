d9324717287549bcce1771abb6361b53728156ae
hsz75 <72518650+hsz75@users.noreply.github.com>
2021-02-02 00:03:22 +0800

    Create 5_information.js

---
+++ w/after/5_information.js
@@ -0,0 +1,15 @@
+var w=5;
+var wtitle='电脑音乐发布直播精彩回放（';
+var year=2021;
+var date=2;
+var day=1;
+var inw='精 彩 断 线<br>多 重 鼠 标<br>另 外 插 播<br>精 彩 失 误<br>不 见 不 散'
+var ty=1; 
+var by='笔稽菌<br>可能会发到B站（视频版）';
+var la='la';
+document.write('今天是高产...吧');
+document.getElementById("1").src = "/f/1.jpg";
+document.getElementById("2").src = "/f/2.jpg";
+document.getElementById("3").src = "/f/3.jpg";
+document.getElementById("4").src = "/f/4.jpg";
+document.getElementById("myImage").src = "/f/5.jpg";
