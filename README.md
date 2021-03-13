7823a559f7631125c943e4adf9252a3679fbb236
hsz75 <hsz75@qq.com>
2021-03-14 01:38:16 +0800

    update

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# hsz75.github.io
-<h3>这里是笔稽DE网站!</h3>
+## hsz75.github.io ##
+##这里是笔稽DE网站!##
 <br>
 <a href="https://hsz75.github.io" target="_blank">笔稽DE网站</a>
 <br>
 <p>Powered&nbsp;by&nbsp;
   <a href="https://hsz75.github.io" target="_blank">
     hsz75</a>
-</p>
--- f/j/flowers.js
+++
@@ -1,7 +0,0 @@
-var system ={}; 
- var p = navigator.platform; 
- system.win = p.indexOf("Win") == 0; 
- system.mac = p.indexOf("Mac") == 0; 
- system.x11 = (p == "X11") || (p.indexOf("Linux") == 0); 
- $.getScript("https://cdn.jsdelivr.net/gh/ivampiresp/special-JavaScript/yinghua.js");
- }
