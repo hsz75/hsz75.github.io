dc8b200abc5aaab105c585048a7458d56f0e00f8
hsz75 <hsz75@qq.com>
2021-05-23 22:36:32 +0800

    update

--- files/js/log.js
+++ files/js/log.js
@@ -1,4 +1,3 @@
 console.log("Copyright 2020-2021 hsz75 All Right Reserved. ")
-console.warn("Without permission, it is forbidden to carry any code of this website!")
-console.warn("这是一个警告，也是一个彩蛋")
-console.warn("未经允许，禁止复制代码")
+console.warn("未经允许!，禁止复制代码")
+console.warn("Don't copy this code of this website!")
