7589ac029cf2b18e6c8efdd293b960dfd9f57177
hsz75 <hsz75@qq.com>
2021-05-23 22:42:24 +0800

    update

--- files/js/w.js
+++ files/js/w.js
@@ -2,5 +2,5 @@ window.onfocus=function (){
 document.title=title;
 };
 window.onblur=function () {
-document.title = 'ä½ çœ‹ä¸è§æˆ‘(:ï¼“ã£ã€€)ã¸'+title; 
+document.title = 'Äã¿´²»¼ûÎÒ(:£³¤Ã¡¡)¤Ø'+" _ "title; 
 };
