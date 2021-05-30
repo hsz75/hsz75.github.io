a1ea78039bfde7be1a501eed84ca25f7c9fa16ca
hsz75 <hsz75@qq.com>
2021-05-30 22:49:14 +0800

    update

--- files/js/log.js
+++ files/js/log.js
@@ -1,3 +1,3 @@
-console.log("Copyright 2020-2021 hsz75 All Right Reserved. ")
+console.log("Copyright 2018-2021 hsz75 All Right Reserved. ")
 console.warn("未经允许!，禁止复制代码")
 console.warn("Don't copy this code of this website!")
