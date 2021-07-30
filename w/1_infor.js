a385cfd58162f6fea64188c3e2a9a601757d7563
hsz75 <hsz75@qq.com>
2021-07-30 17:22:10 +0800

    添加之前的文章

---
+++ w/1_infor.js
@@ -0,0 +1,6 @@
+var w=1;
+var wtitle='hello,world';
+var year=2020
+var date=10;
+var day=8;
+var inw='hello,world'
---
+++ w/5_infor.js
@@ -0,0 +1,7 @@
+var w=6;
+var wtitle='祝大家2021年新年快乐！';
+var year=2021;
+var date=2;
+var day=12;
+var inw='祝大家2021年新年快乐！'
+var by='笔稽菌';
