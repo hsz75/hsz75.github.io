07af8c8c9ecd9f4a7cf043648a4ea96cbda902a0
hsz75 <hsz75@qq.com>
2021-08-25 23:28:32 +0800

    更新主页文件

--- files/js/w.js
+++ files/js/w.js
@@ -1,4 +1,11 @@
-﻿window.onfocus=function (){
+﻿var w_list=new Array[5]
+w_list[0]="Hello,world"
+w_list[1]="2020啊(总结)"
+w_list[2]="这只是一个提醒"
+w_list[3]="电脑音乐发布直播精彩回放（ "
+w_list[4]="祝大家2021年新年快乐!"
+
+window.onfocus=function (){
 document.title=title;
 };
 window.onblur=function () {
