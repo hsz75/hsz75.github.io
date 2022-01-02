bfeee0221ce07b9dbc5ec63da050ea410739cb78
hsz75 <hsz75@qq.com>
2022-01-02 19:13:22 +0800

    大更新

--- w/2_infor.js
+++
@@ -1,8 +0,0 @@
-﻿var w=2;
-var wtitle='2020啊';
-var year=2021
-var date=1;
-var day=28;
-var inw='大家好 （手动滑稽）<br>我来了<br>2020年<br>我只发了一个视频，只有1000+播放量，3条弹幕<br>但是这些都是无关紧要的<br>2020年我一年都在学习html和css，虽然效果不好，但是至少能看了（ <br><br><center><img src="/files/image/un.jpg" height="233" width="233" /><font size="2.33"><br>又不是不能用.jpg</font></center><br><br>2020年上半年我还搭建了mc服务器生和原，虽然现在已经关服了，但是这让我更进一步的了解内网穿透<br>也感谢@电脑音乐发布在2020年对我的大力支持<br><br>下半年......<br>过得就有点......反正你们也知道......我就不说了<br><br><br>希望2021年过得更好！<br><br><br>'
-var by='笔稽菌';
-
