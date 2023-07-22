e4c6b4037b0368ec9a49d50fab2cf744c1d362e0
hsz75 <hsz75@qq.com>
2023-07-22 16:47:56 +0800

    更新主页，新增小事记

--- files/js/content.js
+++ files/js/content.js
@@ -1,3 +1,3 @@
-viedo_1v.innerHTML = "揭秘十多年前的木马的发作过程！！";
-document.getElementById("jump_1v").href="https://www.bilibili.com/video/BV1et4y1378y";
-document.getElementById("jump_1v").target="_blank"
+var viedo=["揭秘十多年前的木马的发作过程！！"];
+//document.getElementById("jump_1v").href="https://www.bilibili.com/video/BV1et4y1378y";
+//document.getElementById("jump_1v").target="_blank"
