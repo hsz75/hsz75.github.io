af02ca0d9282c7598377dd09f34142ae0fdd30ae
hsz75 <hsz75@qq.com>
2023-10-21 01:31:33 +0800

    更新向导

--- files/js/myfunc.js
+++ files/js/myfunc.js
@@ -2,15 +2,17 @@ var step1 = document.getElementById("step1");
 var step2 = document.getElementById("step2");
 var step3 = document.getElementById("step3");
 var step4 = document.getElementById("step4");
+var test_bu = document.getElementById("test");
+var test_txt=document.getElementById("test_bu");
 var button = document.getElementById("up");
 var b2 = document.getElementById("down");
 var num = 1;
 
 
-    step2.style.display = 'none';
-    step3.style.display = 'none';
-    step4.style.display = 'none';
-    button.style.display = "none";
+step2.style.display = 'none';
+step3.style.display = 'none';
+step4.style.display = 'none';
+button.style.display = "none";
 
 
 function tostep() {
@@ -56,12 +58,18 @@ function upstep() {
 
             break;
 
-            case 4:
-                step4.style.display = "none";
-                step3.style.display = "inline";
-                b2.style.display = "inline";
-                num--;
-    
-                break;
+        case 4:
+            step4.style.display = "none";
+            step3.style.display = "inline";
+            b2.style.display = "inline";
+            num--;
+
+            break;
     }
 }
+
+function test() {
+    test_bu.innerHTML="<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ExqqdUXXdgA?si=ssodp56KtWwcGK1d\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\"></iframe><br><iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/I1At6oBpAPo?si=XNtzBb5Sc8ibNZaf\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+    test_txt.innerHTML="如果不行……<br>点我重试<br>（最好检查软件后再试）"
+}
+
