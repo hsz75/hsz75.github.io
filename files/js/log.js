3154778ef24dbcb32cc2f5dd3694c7a628d390f7
hsz75 <hsz75@qq.com>
2024-07-29 14:21:00 +0800

    更新网站细节

--- files/js/log.js
+++ files/js/log.js
@@ -1,3 +1,3 @@
-﻿console.info("Copyright 2018-2021 hsz75 All Right Reserved. ")
+﻿console.info("Copyright 2018-2024 hsz75 All Right Reserved. ")
 console.log("未经允许!，禁止复制代码")
 console.log("Don't copy this code of this website!")
